e78cf2077d475c3b1d80186164865f56799e0790
vanessa olivos <veolivos90@gmail.com>
2026-04-07 21:49:06 -0700

    fixing some errors that came from out-of-order merge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./pages/AuthContext.jsx";
-import { AuthProvider } from "./pages/AuthContext.jsx";
-import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
